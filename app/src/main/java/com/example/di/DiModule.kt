0ac9e11dd5416c679ea9b1336dad5261098d280a
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2023-07-24 15:32:54 +0200

    Hilt di establishment

---
+++ app/src/main/java/com/example/di/DiModule.kt
@@ -0,0 +1,10 @@
+package com.example.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DiModule {
+}
